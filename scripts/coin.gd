c768eccdc6f737f9b8671b406bd6e4a56706967c
Ethan Pinto <ethanpintoa@gmail.com>
2024-07-14 18:25:04 -0400

    Add music and coin pickup sound

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,8 +1,9 @@
 extends Area2D
 
 @onready var game_manager := %GameManager
+@onready var animation := $AnimationPlayer
 
 func _on_body_entered(_body: Node2D):
 	print("Coin collected")
 	game_manager.increment_score()
-	self.queue_free()
+	animation.play("pickup")
